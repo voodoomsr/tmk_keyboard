e7b77259270a3ac14de927168c43d420e8d7b829
Michael Soza <mjsr@outlook.com>
2017-12-04 17:31:29 -0300

    Disabled space as shift due to problem with fast typing

--- keyboard/hhkb/keymap_ph.c
+++ keyboard/hhkb/keymap_ph.c
@@ -12,11 +12,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
 
     [0] = \
-    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLASH,GRAVE, \
+    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLASH, FN4, \
            TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,CAPS, \
            LCTL, A,   S,   D,   F,   G,   H,   J,   K,   L,   FN2, FN3, LCTL, \
            LGUI, Z,   X,   C,   V,   B,   N,   M, COMMA, DOT, SLASH, LGUI,FN1, \
-                LSFT,LALT,           FN5,                LALT, FN4),
+                LALT,LSFT,           SPACE,                LSFT, LALT),
 
     [1] = \
     KEYMAP(FN0, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
@@ -62,7 +62,6 @@ const action_t fn_actions[] PROGMEM = {
     [2] = ACTION_LAYER_TAP_TOGGLE(2), 
     [3] = ACTION_LAYER_TAP_TOGGLE(3),
     [4] = ACTION_LAYER_TAP_TOGGLE(4),
-    [5] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_SPACE),
 		[10] = ACTION_MODS_KEY(MOD_LSFT, KC_1),          //Exclamation
 		[11] = ACTION_MODS_KEY(MOD_LSFT, KC_2),          //At
 		[12] = ACTION_MODS_KEY(MOD_LSFT, KC_3),          //Sharp
