ef7e871052ccd3c99c88842167835f42d28fb666
Michael Soza <mjsr@outlook.com>
2018-01-15 21:28:09 -0300

    Increased timeout to use tab more comfortably

--- keyboard/hhkb/config.h
+++ keyboard/hhkb/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* period of tapping(ms) */
-#define TAPPING_TERM    150
+#define TAPPING_TERM    500
 /* tap count needed for toggling a feature */
 #define TAPPING_TOGGLE  5
 /* Oneshot timeout(ms) */
