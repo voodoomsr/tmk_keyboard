eb77d6a4c2be16db0692c6326880f326c6d8e257
Michael Soza <mjsr@outlook.com>
2018-01-29 22:58:37 -0300

    Normalized numeric asignation, previous design wasnt ergonomic enough

--- keyboard/hhkb/keymap_ph.c
+++ keyboard/hhkb/keymap_ph.c
@@ -12,39 +12,39 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
 
     [0] = \
-    KEYMAP(CAPS, FN6, FN7,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11, F12, F1, F2, \
+    KEYMAP(FN7, 1,   2,   3,   4,  5,   6,   7,   8,   9,   0,  F11, F12, F1, FN0, \
            TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,TAB, \
            LCTL, A,   S,   D,   F,   G,   H,   J,   K,   L,   FN4, QUOT, LCTL, \
            LGUI, Z,   X,   C,   V,   B,   N,   M, COMMA, DOT, SLASH, LGUI,FN5, \
-                LALT, LSFT,           SPACE,                LALT, FN0),
+                LALT, LSFT,           SPACE,                LALT, FN6),
 
     [2] = \
-    KEYMAP(TRNS, TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS, TRNS, TRNS, TRNS, TRNS, \
+    KEYMAP(TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS, FN0, \
            TRNS,NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS, UP,  NO,  BSPC, \
            TRNS,VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,LEFT,RGHT,ENT, \
            TRNS,NO,  NO,  NO,  NO,  NO,  PPLS,PMNS,END, PGDN,DOWN, TRNS,TRNS, \
-                TRNS,TRNS,           TRNS,               TRNS, FN0),
+                TRNS,TRNS,           TRNS,               TRNS, TRNS),
 
     [3] = \
-    KEYMAP(TRNS, PGDN, PGUP,  HOME,  END,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS, TRNS, TRNS, TRNS, TRNS, \
-           TRNS, ESC,  BSPC,   UP,  DEL, NO, NO, NO,     LGUI,   NO, NO, NO, NO,  TRNS, \
-           TRNS, ENT, LEFT, DOWN, RGHT, TAB, NO, LALT, LCTL, TRNS, TRNS, TRNS, TRNS, \
-           TRNS, QUOT, FN27, SCOLON, FN26, SPACE,  NO, NO, NO, TRNS, TRNS, TRNS,TRNS, \
-                TRNS,TRNS,          TAB,                TRNS, FN0),
+    KEYMAP(TRNS, PGDN, PGUP,  HOME,  END,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS, TRNS, TRNS, TRNS, FN0, \
+           TRNS, ESC,  BSPC,   UP,  DEL,     NO, NO, NO,     LGUI,   NO, NO, NO, NO,  TRNS, \
+           TRNS, ENT, LEFT,  DOWN, RGHT,      NO,   NO, LALT, LCTL, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, MINS, EQL, SCOLON, FN26, TRNS,  NO, NO, NO, TRNS, TRNS, TRNS,TRNS, \
+                TRNS,TRNS,          TRNS,                TRNS, TRNS),
 
     [4] = \
-    KEYMAP(TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS, \
+    KEYMAP(TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    TRNS,    FN0, \
            TRNS,  NO,    NO,  TRNS,  TRNS,   TRNS,   TRNS,  FN24,  FN25,  LBRC,  RBRC,     TRNS,   TRNS,   TRNS, \
            TRNS,  NO,  TRNS,  TRNS,  TRNS,   TRNS,   TRNS,  MINS,   EQL,BSLASH, GRAVE,     TRNS,   TRNS, \
            TRNS,TRNS,  TRNS,  TRNS,  TRNS,   TRNS,   TRNS,  FN20,  FN21,  FN22,  FN23,     TRNS,   TRNS, \
-                TRNS,TRNS,           TAB,              TRNS, FN0),
+                TRNS,TRNS,           TRNS,              TRNS, TRNS),
 
 		[5] = \
-    KEYMAP(TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  TRNS ,  TRNS, BSPC, TRNS, F12, INS, FN3, \
-           TRNS, NO,  NO,  NO,  NO,  NO,    NO,   7,  8,   9,   TRNS,   TRNS, TRNS,   BSPC,  \
-           TRNS, NO,  NO,  NO,  NO,  NO,   MINS,   4,  5,   6,   FN21,  TRNS,  ENTER, \ 
-           TRNS, NO,  NO,  NO,  NO,  TRNS,  TRNS,   1,  2,   3,   TRNS,   TRNS, TRNS, \ 
-                TRNS,TRNS,         0,               TRNS      ,  FN0),
+    KEYMAP(TRNS, F1,  F2,  F3,  F4,  F5,  F6,  F7,  TRNS ,  FN18, FN19, TRNS, F12, INS, FN0, \
+           TRNS, ESC,  BSPC,   UP,  DEL,    NO,    NO,   7,  8,   9,   FN17,   TRNS, TRNS,  TRNS,  \
+           TRNS, ENT, LEFT,  DOWN, RGHT,    NO,   TRNS,   4,  5,   6,   FN21,  TRNS,  TRNS, \ 
+           TRNS, MINS, EQL, SCOLON, FN26,  TRNS,  TRNS,   1,  2,   3,   TRNS,   TRNS, TRNS, \ 
+                TRNS,TRNS,         0,               TRNS      ,  TRNS),
 
 	};
 
@@ -59,7 +59,7 @@ const action_t fn_actions[] PROGMEM = {
     [0] = ACTION_LAYER_SET_CLEAR(0),
 	  [4] = ACTION_LAYER_MOMENTARY(3),
     [5] = ACTION_LAYER_MOMENTARY(2), 
-		[6] = ACTION_LAYER_MOMENTARY(5),
+		[6] = ACTION_LAYER_TOGGLE(5),
     [7] = ACTION_LAYER_MOMENTARY(4), 
   	[10] = ACTION_MODS_KEY(MOD_LSFT, KC_1),          //Exclamation
 		[11] = ACTION_MODS_KEY(MOD_LSFT, KC_2),          //At
